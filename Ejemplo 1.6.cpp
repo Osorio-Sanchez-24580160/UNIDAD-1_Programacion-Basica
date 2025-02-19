6e6a4e62e35078b832291d8874c1ead8dd21cb7c
Osorio-Sanchez-24580160 <omarosorio553@gmail.com>
2025-02-19 11:43:27 -0600

    Add files via upload

---
+++ Ejemplo 1.6.cpp
@@ -0,0 +1,31 @@
+#include "iostream"
+#include "string"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hola! Este programa 1.6 Escribe los datos en orden inverso" << "\n";
+	
+	int A,B,C,D;
+	
+	cout << "Por favor ingrese el primer valor A:   " << "\n";
+	
+	cin >> A; 
+	
+	cout << "Por favor ingrese el segundo valor B:   " << "\n";
+	
+	cin >> B; 
+	
+	cout << "Por favor ingrese el tercer valor C:   " << "\n";
+	
+	cin >> C; 
+	
+	cout << "Por favor ingrese el cuarto valor D:   " << "\n";
+	
+	cin >> D;
+	
+	cout << D << " , " << C << " , " << B << " , " << A; 
+	
+	return 0;
+}
